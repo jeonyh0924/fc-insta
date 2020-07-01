7e6edc0803706c4a8e0fc83d8e540e94c2196492
Jeon Young Hoon <hungyb0924@gmail.com>
2020-07-01 10:25:15 +0900

    first commit, settings 완료(docker-compose, redis, postgres)

---
+++ app/members/models.py
@@ -0,0 +1,36 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+# Create your models here.
+class User(AbstractUser):
+    email = models.EmailField(unique=True, )
+    relation_users = models.ManyToManyField(
+        'self',
+        through='Relations',
+        related_name='+',
+        symmetrical=False,
+        blank=True,
+    )
+
+
+class Relations(models.Model):
+    CHOICE_RELATIONS_TYPE = (
+        ('f', 'follow'),
+        ('b', 'block'),
+    )
+    from_user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='from_user_relations',
+        related_query_name='from_users_relation',
+    )
+    to_user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='to_user_relations',
+        related_query_name='to_users_relation',
+    )
+    related_type = models.CharField(
+        choices=CHOICE_RELATIONS_TYPE,
+    )
