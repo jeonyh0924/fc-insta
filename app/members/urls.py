ae082a0c079d4b19a12989964d6676b538a935f0
Jeon Young Hoon <hungyb0924@gmail.com>
2020-07-02 19:40:45 +0900

    feature:user-follow&follower model-create & delete block relation

--- app/members/urls.py
+++ app/members/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from rest_framework import routers
 
-from members.views import UserModelViewAPI, RelationAPIView
+from members.views import UserModelViewAPI
 
 router = routers.SimpleRouter(trailing_slash=False)
-router.register('relation', RelationAPIView)
+
 router.register(r'user', UserModelViewAPI)
 
 urlpatterns = router.urls
--- app/members/views.py
+++ app/members/views.py
@@ -13,9 +13,10 @@ User = get_user_model()
 
 class UserModelViewAPI(viewsets.ModelViewSet):
     queryset = User.objects.all()
+    serializer_class = UserSerializers
 
     def get_serializer_class(self):
-        if self.action == 'makeFollow':
+        if self.action in ['makeFollow', 'makeBlock']:
             return RelationSerializers
         return super().get_serializer_class()
 
@@ -67,11 +68,11 @@ class UserModelViewAPI(viewsets.ModelViewSet):
         return Response(serializers.data, status=status.HTTP_200_OK)
 
     @action(detail=False, methods=['post'])
-    def makeFollow(self, request):
+    def makeBlock(self, request):
         to_user = User.objects.get(pk=request.query_params.get('toUser'))
         relation_type = request.query_params.get('type')
         try:
-            Relations.objects.get(from_user=request.user, to_user=to_user)
+            relation = Relations.objects.get(from_user=request.user, to_user=to_user)
         except Relations.DoesNotExist:
             data = {
                 'from_user': request.user.pk,
@@ -83,19 +84,43 @@ class UserModelViewAPI(viewsets.ModelViewSet):
                 serializer.save()
                 return Response(serializer.data, status=status.HTTP_201_CREATED)
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        return Response({'message': "exists relation"}, status=status.HTTP_400_BAD_REQUEST)
+        relation.related_type = 'b'
+        relation.save()
+        return Response({'message': 'change relation'}, status=status.HTTP_200_OK)
 
     @action(detail=False, methods=['delete'])
-    def deleteFollow(self, request):
+    def deleteBlock(self, request):
+        to_user = User.objects.get(pk=request.query_params.get('toUser'))
         try:
-            relation = Relations.objects.get(from_user=request.user,
-                                             to_user=User.objects.get(pk=request.query_params.get('toUser')))
+            relation = Relations.objects.get(from_user=request.user, to_user=to_user)
+        except Relations.DoesNotExist:
+            return Response({'message: has not Relation'}, status=status.HTTP_400_BAD_REQUEST)
+        relation.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    @action(detail=False, )
+    def create_delete_Relation(self, request):
+        """
+        :param request: relation type 이 f면 팔로우 해주고  b면 블락건다.
+
+        1. 이미 팔로우 한 유저가 블락을 걸면 이미 존재하는 릴레이션 지우고 사용자의 요청에 맞게 해준다.
+
+        :return:
+        """
+        to_user = User.objects.get(pk=request.query_params.get('toUser'))
+        relation_type = request.query_params.get('type')
+        try:
+            relation = Relations.objects.get(from_user=request.user, to_user=to_user)
             relation.delete()
             return Response(status=status.HTTP_204_NO_CONTENT)
         except Relations.DoesNotExist:
-            return Response({'message': " not exists relation"}, status=status.HTTP_400_BAD_REQUEST)
-
-
-class RelationAPIView(viewsets.ModelViewSet):
-    queryset = Relations.objects.all()
-    serializer_class = RelationSerializers
+            data = {
+                'from_user': request.user.pk,
+                'to_user': to_user.pk,
+                'related_type': relation_type
+            }
+            serializer = self.get_serializer(data=data)
+            if serializer.is_valid(raise_exception=True):
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
